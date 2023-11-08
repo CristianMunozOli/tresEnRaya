f56760a512f9026464842fcf4a131a95b6999c4f
Cristian <230183@fppiramide.com>
2023-11-08 08:55:38 +0100

    initial

---
+++ src/test/java/com/cristian/TresEnRaya/TresEnRayaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cristian.TresEnRaya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TresEnRayaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
